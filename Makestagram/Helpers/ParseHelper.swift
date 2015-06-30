c3439cf4ec9a4d4784906c8837fa095d4368a133
Petrina Koh <petrinakoh21@gmail.com>
2015-06-30 16:56:57 -0700

    Add Parse Helper Class

---
+++ Makestagram/Helpers/ParseHelper.swift
@@ -0,0 +1,34 @@
+//
+//  ParseHelper.swift
+//  Makestagram
+//
+//  Created by Petrina Koh on 6/30/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+// 1 wrap all helper methods in class ParseHelper
+class ParseHelper {
+    
+    // 2 static method - can call it without having to create an instance of ParseHelper
+    // by taking the callback as a parameter, we can call any Parse method and return the result of the method to that completionBlock
+    static func timelineRequestforCurrentUser(completionBlock: PFArrayResultBlock) {
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo: PFUser.currentUser()!)
+        
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        query.includeKey("user")
+        query.orderByDescending("createdAt")
+        
+        // 3 whoever calls the timelineRequestforCurrentUser method will be able to handle the result returned from the query
+        query.findObjectsInBackgroundWithBlock(completionBlock)
+    }
+}
