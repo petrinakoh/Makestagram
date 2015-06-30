94429b819a063ca77ce77b7d36eb54f75f87d475
Petrina Koh <petrinakoh21@gmail.com>
2015-06-30 09:44:03 -0700

    Connect to Parse Backend, Set up App Structure with UITabBarController, Implement Photo Source Selection Popover

---
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TimelineViewController.swift
+//  Makestagram
+//
+//  Created by Petrina Koh on 6/29/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+    
+    var photoTakingHelper: PhotoTakingHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarController?.delegate = self
+    }
+    
+    func takePhoto() {
+        // instantiate photo taking class, provide callback for when photo is selected
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+            // dont do anything yet..
+        }
+    }
+
+}
+
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+    }
+    
+}
